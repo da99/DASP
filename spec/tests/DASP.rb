77631ab32111151ba4cc35a315c2f6d125f56e47
da99 <i-hate-spam-45671204@mailinator.com>
2012-02-22 07:00:19 -0600

    Added: specs for :contains, :map

--- spec/tests/DASP.rb
+++ spec/tests/DASP.rb
@@ -16,3 +16,21 @@ describe "DASP.new" do
   
 end # === DASP.new
 
+describe "DASP ARRAY map <method>" do
+  
+  it "executes method for each item in the ARRAY" do
+    DASP.new([ " a ", " b ", " c "], 'strip').map
+    .should == %w{ a b c }
+  end
+  
+end # === DASP ARRAY map <method>
+
+describe "DASP ARRAY contains word INPUT" do
+  
+  it "selects elements that contain <word>" do
+    DASP.new( %w{ STR_a STR_b NO_c }, 'STR').contains
+    .should == %w{ STR_a STR_b }
+  end
+  
+end # === DASP <to array> contains word INPUT
+
